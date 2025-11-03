6234c0f04b5c74329067a9e41d05fda26777b520
Ariunbold <ariunbold@MacBook-Pro-Ariunbold.local>
2025-11-04 03:46:54 +0800

    day1.5

--- src/app/page.js
+++ src/app/page.js
@@ -2,35 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <div className="border p-4 rounded-b-md w-[450px] h-[600px] bg-white">
+    <div className="flex min-h-screen justify-center p-3 bg-gray-50">
+      <div className="border p-4 rounded-b-md w-[400px] h-[200px]  bg-white">
         <div className="flex flex-col items-center font-sans gap-[40px] ">
-          <img
-            className=" rounded-full w-[100px]"
-            alt="profile-img"
-            src="image.png"
-          />
+          <div className=" text-black font-bold text-sm ">To-Do List</div>
+          <div className=" flex flex-row gap-3 w-full ">
+          <input
+  type="text"
+  placeholder="Add a new task..."
+  className=" children rounded-[8px] border w-full h-[40px] px-3 py-2 text-gray-700 placeholder-gray-600 text-sm outline-none"
 
-          <h1 className=" font-sans text-black">Flipflop Dugdag</h1>
-          <h2 className=" text-gray-400">Free time Hero @ Gotham</h2>
-          <h2 className=" text-center text-gray-400">
-            my special skill is kick extremely fast.I'm faster than Usan Bolt
-            and stronger than Hulk.but some people say you're too skinny but
-            it's not a problem for my job.
-          </h2>
-
-          <div className="flex w-80 h-10 text-center bg-emerald-500 rounded-md text-white items-center justify-center hover:bg-amber-100">
-            Contact Meee
+/>
+          <button className=" rounded-[8px] bg-blue-500 w-[60px] text-white text-sm ">Add</button>
           </div>
-          <div className=" flex gap-10 justify-self-end">
-            <img className="w-6 h-6"
-            src="image copy.png"></img>
-            <img className="w-6 h-6"
-            src="image copy.png"></img>
-            <img className="w-6 h-6"
-            src="image copy.png"></img>
-            <img className="w-6 h-6"
-            src="image copy.png"></img>
+
+          <div className=" flex gap-2 justify-center">
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
           </div>
         </div>
       </div>
